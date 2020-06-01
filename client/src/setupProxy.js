e1f9a9c13ab381446d9825b32a37d67d932f14db
Iryna Matviichuk <ira.mia.m99@gmail.com>
2020-06-01 16:38:58 +0300

    setup proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -5,70 +5,70 @@ module.exports = app => {
     app.use(
         '/genre/*',
         createProxyMiddleware({
-            target: 'http://localhost:8081',
+            target: 'http://search-service:8081',
             changeOrigin: true,
         })
     ),
     app.use(
         '/auditorium/*',
         createProxyMiddleware({
-            target: 'http://localhost:8081',
+            target: 'http://search-service:8081',
             changeOrigin: true,
         })
     ),
     app.use(
         '/movie/*',
         createProxyMiddleware({
-            target: 'http://localhost:8081',
+            target: 'http://search-service:8081',
             changeOrigin: true,
         })
     ),
     app.use(
         '/screening/*',
         createProxyMiddleware({
-            target: 'http://localhost:8081',
+            target: 'http://search-service:8081',
             changeOrigin: true,
         })
     ),
     app.use(
         '/auditorium/*',
         createProxyMiddleware({
-            target: 'http://localhost:8081',
+            target: 'http://search-service:8081',
             changeOrigin: true,
         })
     ),
     app.use(
         '/booking/*',
         createProxyMiddleware({
-            target: 'http://localhost:8081',
+            target: 'http://search-service:8081',
             changeOrigin: true,
         })
     ),
     app.use(
         '/book/*',
         createProxyMiddleware({
-            target: 'http://localhost:8082',
+            target: 'http://booking-service:8082',
             changeOrigin: true,
         })
     ),
     app.use(
         '/auth/*',
         createProxyMiddleware({
-            target: 'http://localhost:8083',
+            target: 'http://authentication-service:8083',
             changeOrigin: true,
         })
     ),
     app.use(
         '/feedback/*',
         createProxyMiddleware({
-            target: 'http://localhost:8084',
+            target: 'http://feedback-service:8084',
             changeOrigin: true,
         })
     ),
     app.use(
         '/admin/*',
         createProxyMiddleware({
-            target: 'http://localhost:8085',
+            target: 'http://admin-service:8085',
             changeOrigin: true,
         })
     )
